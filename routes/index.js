28cb3ade1c1dad49769f54c5944e9c04a7dce9fc
willbyrne <will.bl.byrne@gmail.com>
2022-12-09 13:32:41 +0000

    POST route & model working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { getMembersByGroupName } from "../models/index.js";
+import { getMembersByGroupName, addGroup } from "../models/index.js";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
     console.log(req.body.groupName)
     const result = await getMembersByGroupName(req.body.groupName);
+    // if (result == undefined) {
+    //   return res.json({ success: false });
+    // }
+    return res.json({ success: true, payload: result });
+  });
+
+router.post("/", async function (req, res) {
+    console.log(req.body.payload)
+    const result = await addGroup(req.body.payload);
     // if (result == undefined) {
     //   return res.json({ success: false });
     // }
