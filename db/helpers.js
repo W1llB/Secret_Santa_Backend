80c4447775d80399e180af0678542e3815fb71ac
willbyrne <will.bl.byrne@gmail.com>
2023-04-23 10:47:56 +0100

    forgot to add all files

--- db/helpers.js
+++ db/helpers.js
@@ -1,3 +1,20 @@
+import { pool } from "./index";
 // create table
+export async function createTable() {
+  console.log("Table created.");
+}
+
 // droptable
+export async function resetTable() {
+  console.log("Table dropped.");
+}
+
 // resetTable
+export async function dropTable() {
+  console.log("Table reset.");
+}
+
+//seed table
+export async function seedTable() {
+  console.log("Seed data.");
+}
